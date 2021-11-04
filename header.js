2f27551991815746e418ec07a055330f369a9fdf
theErre <rubenmachado909@gmail.com>
2021-11-04 18:55:08 -0300

    mejoras en header

--- header.js
+++ header.js
@@ -1,9 +1,23 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 0) {
-            $('header').addClass('header2');
-        } else {
-            $('header').removeClass('header2');
-        }
-    });
-});
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
